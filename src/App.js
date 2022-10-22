3691bc512edd66d256885b724aee260a32832cc0
Luis Manuel Palmera Quintero <lmpalmera04@gmail.com>
2022-10-22 12:15:05 -0500

    frontend

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import data from "data.json";
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <a href="/">eSHOP</a>
+      </header>
+        <main>
+          list product
+        </main>
+    </div>
+  );
+}
+
+export default App;
