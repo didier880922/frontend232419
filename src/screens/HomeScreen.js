26d4d453c1625d4e145029450c33c50931d6fe02
Luis Manuel Palmera Quintero <dfguerrero@unicesar.edu.co>
2022-10-29 15:13:33 -0500

    sabado
    sabado

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,14 +1,38 @@
-import { useEffect, useState } from "react";
+import { useEffect, useReducer } from "react";
 import { Link } from "react-router-dom";
 import axios from 'axios';
+import logger from 'use-reducer-logger';
 //import data from "../data";
 
+const reducer = (state, action) => {
+  switch(action.type) {
+    case 'FETCH_REQUEST':
+      return {...state, loading: true};
+    case 'FETCH_SUCCESS':
+      return {...state, products: action.payload, loading: false};
+    case 'FETCH_FAIL':
+      return {...state, loading: false, error: action.payload};
+    default:
+      return state;
+  }
+};
+
 function HomeScreen() {
-  const [products, setProducts] = useState([]);
+  const [{loading, error, products}, dispatch] = useReducer(logger(reducer), {
+    products: [],
+    loading: true, error: '',
+  });
+
+  //const [products, setProducts] = useState([]);
   useEffect(() => {
     const fecthData = async () => {
-      const result = await axios.get('http://localhost:5000/api/products');
-      setProducts(result.data);
+      dispatch({type: 'FETCH_REQUEST'});
+      try {
+        const result = await axios.get('http://localhost:5000/api/products')
+        dispatch({type: 'FETCH_SUCCESS', payload: result.data });
+      } catch (err) {
+        dispatch({type: 'FETCH_FAIL', payload: err.message});
+      }
     };
     fecthData();
   }, []);
@@ -16,7 +40,12 @@ function HomeScreen() {
     <div>
       <h1>Lista de productos</h1>
       <div className="products">
-        {products.map((product) => (
+        {loading ? (
+             <div>Cargando datos...</div>
+          ) : error ? ( 
+          <div>{error}</div>
+          ) : (
+        products.map((product) => (
           <div className="product" key={product.slug}>
             <Link to={`/product/${product.slug}`}>
               <img src={product.image} alt={product.name} />
@@ -29,7 +58,8 @@ function HomeScreen() {
               <button>Agregar al carrito</button>
             </div>
           </div>
-        ))}
+        ))
+      )}
       </div>
     </div>
   );
